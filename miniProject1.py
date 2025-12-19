a74d1ebba81662ddf86ff7d8ebeb206fb469c667
sp89173 <parvathibommathoti734@gmail.com>
2025-12-19 14:44:51 +0530

    Create miniProject1.py

---
+++ miniProject1.py
@@ -0,0 +1,28 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/LabUser/AppData/Local/Programs/Python/Python36-32/mini project  1.py 
+welcome to the SBI
+Traceback (most recent call last):
+  File "C:/Users/LabUser/AppData/Local/Programs/Python/Python36-32/mini project  1.py", line 28, in <module>
+    s.deposit()        # Deposit money
+AttributeError: 'bank' object has no attribute 'deposit'
+>>> 
+===== RESTART: C:/Users/LabUser/Desktop/mini project/mini project  1.py =====
+welcome to the SBI
+Traceback (most recent call last):
+  File "C:/Users/LabUser/Desktop/mini project/mini project  1.py", line 28, in <module>
+    s.deposit()        # Deposit money
+AttributeError: 'bank' object has no attribute 'deposit'
+>>> 
+===== RESTART: C:/Users/LabUser/Desktop/mini project/mini project  1.py =====
+welcome to the SBI
+enter amount to be deposited:5000
+
+Amount Deposited: 5000.0
+Enter amount to be Withdrawn: 2000
+
+You Withdrew: 2000.0
+
+Net Available Bal = 3000.0
+>>> 
